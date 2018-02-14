fc7fc4f7cccb1b38c1aba1484709999eb22e9039
Fernando Henrique Bandeira <fernando.bandeira94@gmail.com>
2018-02-14 14:06:30 -0200

    Add bootstrap 4

--- form/src/index.js
+++ form/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import './include/bootstrap'
+
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
