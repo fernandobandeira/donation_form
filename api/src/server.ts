5ee71f6739672e3b513d45ba3490f83119cd7100
Fernando Henrique Bandeira <fernando.bandeira94@gmail.com>
2018-02-14 23:42:25 -0200

    Add docs

--- api/src/server.ts
+++ api/src/server.ts
@@ -2,7 +2,7 @@ import { App } from './app';
 
 const {
   PORT = 3000,
-  MONGODB_URI = 'mongodb://localhost:27017/DonorsTest',
+  MONGODB_URI = 'mongodb://localhost:27017/Donors',
 } = process.env;
 
 const { app } = new App({ MONGODB_URI });
